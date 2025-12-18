806ff73700122da52c360375f990d048e07a1226
Aman2074 <amanbhadoriya777@gmail.com>
2025-12-18 21:50:08 +0530

    [Aman] : Added program for FactorsofNumber

---
+++ gcr-codebase/control-flow/Level-2-Lab-Practice/FactorsofNumber.java
@@ -0,0 +1,31 @@
+// creating a classFactorsofnumber
+
+import java.util.Scanner;
+
+public class FactorsOfNumber {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        // Take user input
+        System.out.print("Enter a positive integer: ");
+        int number = input.nextInt();
+
+        // Check for positive integer
+        if (number <= 0) {
+            System.out.println("Please enter a valid positive integer.");
+        } else {
+            System.out.println("Factors of " + number + " are:");
+
+            // Loop from 1 to less than number
+            for (int i = 1; i < number; i++) {
+                if (number % i == 0) {
+                    System.out.println(i);
+                }
+            }
+        }
+
+        input.close();
+    }
+}
+
