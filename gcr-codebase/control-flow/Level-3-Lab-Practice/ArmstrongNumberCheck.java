d9decb5470d1d2027cce7ef8fc47cb5d99dea5d6
Aman2074 <amanbhadoriya777@gmail.com>
2025-12-19 14:21:17 +0530

    [Aman] : Added program for ArmstrongNumberCheck'

---
+++ gcr-codebase/control-flow/Level-3-Lab-Practice/ArmstrongNumberCheck.java
@@ -0,0 +1,33 @@
+// Creating a clasArmstrongNumbercheck
+
+import java.util.Scanner;
+
+public class ArmstrongNumberCheck {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+        int sum = 0;
+        int originalNumber = number;
+
+        while (originalNumber != 0) {
+
+            int digit = originalNumber % 10;
+
+            sum = sum + (digit * digit * digit);
+
+            originalNumber = originalNumber / 10;
+        }
+
+        if (sum == number) {
+            System.out.println(number + " is an Armstrong Number");
+        } else {
+            System.out.println(number + " is NOT an Armstrong Number");
+        }
+
+        input.close();
+    }
+}
