97a2310f0ec7542660c4e9b144ef538849f49336
Aman2074 <amanbhadoriya777@gmail.com>
2025-12-19 12:02:13 +0530

    [Aman] : Added program for MultipleBelow100

---
+++ gcr-codebase/control-flow/Level-2-Lab-Practice/MultipleBelow100.java
@@ -0,0 +1,27 @@
+// Creating a classMultipleBelow100
+
+import java.util.Scanner;
+
+public class MultiplesBelow100 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a positive number less than 100: ");
+        int number = input.nextInt();
+
+        if (number > 0 && number < 100) {
+            System.out.println("Multiples of " + number + " below 100:");
+
+            for (int i = 100; i >= 1; i--) {
+                if (i % number == 0) {
+                    System.out.println(i);
+                }
+            }
+        } else {
+            System.out.println("Invalid input! Please enter a positive number less than 100.");
+        }
+
+        input.close();
+    }
+}
