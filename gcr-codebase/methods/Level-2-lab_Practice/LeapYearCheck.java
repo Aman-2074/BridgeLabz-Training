558eae66b1996b5e91a62e89485feceff00e96c4
Aman2074 <amanbhadoriya777@gmail.com>
2025-12-20 18:05:54 +0530

    [Aman] : Added program for LeapYearCheck

---
+++ gcr-codebase/methods/Level-2-lab_Practice/LeapYearCheck.java
@@ -0,0 +1,35 @@
+// Creating a classLeapYearCheck
+
+import java.util.Scanner;
+
+public class LeapYearCheck {
+
+    public static boolean isLeapYear(int year) {
+
+        if (year < 1582) {
+            return false;
+        }
+
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a year: ");
+        int year = input.nextInt();
+
+        if (isLeapYear(year)) {
+            System.out.println(year + " is a Leap Year.");
+        } else {
+            System.out.println(year + " is not a Leap Year.");
+        }
+
+        input.close();
+    }
+}
