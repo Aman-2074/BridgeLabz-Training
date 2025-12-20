b6f3f17686eab325a3ac37c02e510c9d91210000
Aman2074 <amanbhadoriya777@gmail.com>
2025-12-20 17:27:23 +0530

    [Aman] : Added program for SmallestAndLargest

---
+++ gcr-codebase/methods/Level-1-Lab-Practice/SmallestAndLargest.java
@@ -0,0 +1,47 @@
+// Creating a classSmallestAndLargest
+
+import java.util.Scanner;
+
+public class SmallestAndLargest {
+
+    public static void findSmallestAndLargest(int a, int b, int c) {
+
+        int smallest = a;
+        int largest = a;
+
+        if (b < smallest) {
+            smallest = b;
+        }
+        if (c < smallest) {
+            smallest = c;
+        }
+
+        if (b > largest) {
+            largest = b;
+        }
+        if (c > largest) {
+            largest = c;
+        }
+
+        System.out.println("Smallest number is: " + smallest);
+        System.out.println("Largest number is : " + largest);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int num1 = input.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = input.nextInt();
+
+        System.out.print("Enter third number: ");
+        int num3 = input.nextInt();
+
+        findSmallestAndLargest(num1, num2, num3);
+
+        input.close();
+    }
+}
