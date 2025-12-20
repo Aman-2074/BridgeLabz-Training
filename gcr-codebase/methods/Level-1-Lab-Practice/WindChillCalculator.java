082aa9b2de1dd1535338b13f512e98033e6b60ee
Aman2074 <amanbhadoriya777@gmail.com>
2025-12-20 17:43:08 +0530

    [Aman] : Added program for WindChillCalculator

---
+++ gcr-codebase/methods/Level-1-Lab-Practice/WindChillCalculator.java
@@ -0,0 +1,36 @@
+// Creating a classWindChillcalculator
+
+import java.util.Scanner;
+
+public class WindChillCalculator {
+
+    public static double calculateWindChill(double temperature, double windSpeed) {
+
+        double windChill = 35.74
+                + (0.6215 * temperature)
+                + (0.4275 * temperature - 35.75) * Math.pow(windSpeed, 0.16);
+
+        return windChill;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter temperature (in Fahrenheit): ");
+        double temperature = input.nextDouble();
+
+        System.out.print("Enter wind speed (in miles per hour): ");
+        double windSpeed = input.nextDouble();
+
+        double windChill = calculateWindChill(temperature, windSpeed);
+
+        System.out.println(
+            "The Wind Chill Temperature is " + windChill +
+            " for Temperature " + temperature +
+            " and Wind Speed " + windSpeed
+        );
+
+        input.close();
+    }
+}
