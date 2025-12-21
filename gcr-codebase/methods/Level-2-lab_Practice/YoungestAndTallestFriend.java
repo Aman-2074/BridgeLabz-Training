292ca090fa6cadd3c9c614f4303c96f09b9454f5
Aman2074 <amanbhadoriya777@gmail.com>
2025-12-21 15:20:16 +0530

    [Aman] : Added program for YoungestAndTallestFriend

---
+++ gcr-codebase/methods/Level-2-lab_Practice/YoungestAndTallestFriend.java
@@ -0,0 +1,53 @@
+// Creating a classYoungestAndTallestFriend
+
+import java.util.Scanner;
+
+public class YoungestAndTallestFriend {
+
+    public static String findYoungest(String[] names, int[] ages) {
+        int minAgeIndex = 0;
+
+        for (int i = 1; i < ages.length; i++) {
+            if (ages[i] < ages[minAgeIndex]) {
+                minAgeIndex = i;
+            }
+        }
+        return names[minAgeIndex];
+    }
+
+    public static String findTallest(String[] names, double[] heights) {
+        int maxHeightIndex = 0;
+
+        for (int i = 1; i < heights.length; i++) {
+            if (heights[i] > heights[maxHeightIndex]) {
+                maxHeightIndex = i;
+            }
+        }
+        return names[maxHeightIndex];
+    }
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        String[] names = {"Amar", "Akbar", "Anthony"};
+        int[] ages = new int[3];
+        double[] heights = new double[3];
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.print("Enter age of " + names[i] + ": ");
+            ages[i] = input.nextInt();
+
+            System.out.print("Enter height of " + names[i] + " (in cm): ");
+            heights[i] = input.nextDouble();
+        }
+
+        String youngest = findYoungest(names, ages);
+        String tallest = findTallest(names, heights);
+
+        System.out.println("\nYoungest friend is: " + youngest);
+        System.out.println("Tallest friend is: " + tallest);
+
+        input.close();
+    }
+}
